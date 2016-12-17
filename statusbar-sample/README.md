71d52d7517bd52b2811245f1ac94e93c3d80664c
Benjamin Pasero <benjpas@microsoft.com>
2016-12-17 11:33:07 +0100

    Add a sample for status bar contribution (show number of selected lines)

---
+++ statusbar-sample/README.md
@@ -0,0 +1,22 @@
+# References Editor Sample
+
+This is a sample extension that adds a status bar entry showing the current number of selected lines.
+
+It is not intended as a product quality extension.
+
+- Open a text editor
+- Select some lines
+- See the number of selected lines in the status bar
+
+![Print References](https://raw.githubusercontent.com/Microsoft/vscode-extension-samples/master/statusbar-sample/preview.gif)
+
+# How it works, what it shows?
+
+- The extension implements and registers a [`StatusBarItem`](http://code.visualstudio.com/docs/extensionAPI/vscode-api#StatusBarItem) to show the number of selected lines.
+- The extension listens to [`events`](http://code.visualstudio.com/docs/extensionAPI/vscode-api#_window) when the active text editor changes to keep the number of selected lines up to date.
+- Registers a command via `package.json` that will show the number of selected lines as message
+
+# How to run locally
+
+* `npm run compile` to start the compiler in watch mode
+* open this folder in VS Code and press `F5`
---
+++ statusbar-sample/src/extension.ts
@@ -0,0 +1,59 @@
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+
+'use strict';
+
+import { ExtensionContext, StatusBarAlignment, window, StatusBarItem, Selection, workspace, TextEditor, commands } from 'vscode';
+
+export function activate(context: ExtensionContext) {
+    const status = window.createStatusBarItem(StatusBarAlignment.Right, 100);
+    status.command = 'extension.selectedLines';
+    context.subscriptions.push(status);
+
+    context.subscriptions.push(window.onDidChangeActiveTextEditor(e => updateStatus(status)));
+    context.subscriptions.push(window.onDidChangeTextEditorSelection(e => updateStatus(status)));
+    context.subscriptions.push(window.onDidChangeTextEditorViewColumn(e => updateStatus(status)));
+    context.subscriptions.push(workspace.onDidOpenTextDocument(e => updateStatus(status)));
+    context.subscriptions.push(workspace.onDidCloseTextDocument(e => updateStatus(status)));
+
+    context.subscriptions.push(commands.registerCommand('extension.selectedLines', () => {
+        window.showInformationMessage(getSelectedLines());
+    }));
+
+    updateStatus(status);
+}
+
+function updateStatus(status: StatusBarItem): void {
+    let text = getSelectedLines();
+    if (text) {
+        status.text = '$(megaphone) ' + text;
+    }
+
+    if (text) {
+        status.show();
+    } else {
+        status.hide();
+    }
+}
+
+function getSelectedLines(): string {
+    const editor = window.activeTextEditor;
+    let text: string;
+
+    if (editor) {
+        let lines = 0;
+        editor.selections.forEach(selection => {
+            lines += (selection.end.line - selection.start.line + 1);
+        });
+
+        if (lines > 0) {
+            text = `${lines} line(s) selected`;
+        }
+    }
+
+    return text;
+}
+
+export function deactivate() {
+}
