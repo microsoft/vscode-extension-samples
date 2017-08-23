972c12d935d267c03ce79e446b7c10643f2ae9ce
Dirk Baeumer <dirkb@microsoft.com>
2017-08-23 16:40:19 +0200

    Added multi server LSP example

---
+++ multi-lsp-sample/server/src/server.ts
@@ -0,0 +1,31 @@
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+'use strict';
+
+import {
+	createConnection, TextDocuments, ProposedProtocol, TextDocumentSyncKind
+} from 'vscode-languageserver';
+
+let connection = createConnection(ProposedProtocol);
+let documents = new TextDocuments();
+let rootUri: string;
+
+documents.onDidOpen((event) => {
+	connection.console.log(`[Server ${rootUri}] Document opened: ${event.document.uri}`);
+})
+documents.listen(connection);
+
+connection.onInitialize((params) => {
+	rootUri = params.rootUri;
+	connection.console.log(`Server started for folder: ${rootUri}`);
+	return {
+		capabilities: {
+			textDocumentSync: {
+				openClose: true,
+				change: TextDocumentSyncKind.None
+			}
+		}
+	}
+});
+connection.listen();
