4f46a914e896ee031d70696edf6b7c259fc0d4bc
Dirk Baeumer <dirkb@microsoft.com>
2017-09-04 16:09:40 +0200

    Add example to use a single server on a multi workspace folder setup

---
+++ lsp-multi-root-sample/client/src/extension.ts
@@ -0,0 +1,89 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+'use strict';
+
+import * as path from 'path';
+
+import { workspace, Disposable, ExtensionContext, WorkspaceConfiguration } from 'vscode';
+import { 
+	LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, TransportKind, ProposedProtocol, Middleware, CancellationToken,
+	ConfigurationMiddleware, GetConfigurationParams
+} from 'vscode-languageclient';
+
+// The example settings
+interface MultiRootExampleSettings {
+	maxNumberOfProblems: number;
+}
+
+export function activate(context: ExtensionContext) {
+
+	// The server is implemented in node
+	let serverModule = context.asAbsolutePath(path.join('server', 'server.js'));
+	// The debug options for the server
+	let debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
+	
+	// If the extension is launched in debug mode then the debug server options are used
+	// Otherwise the run options are used
+	let serverOptions: ServerOptions = {
+		run : { module: serverModule, transport: TransportKind.ipc },
+		debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions }
+	}
+
+	// Convert VS Code specific settings to a format acceptable by the server. Since
+	// both client and server do use JSON the conversion is trivial. 
+	let configurationMiddleware: ConfigurationMiddleware = {
+		configuration: (params: GetConfigurationParams, _token: CancellationToken, _next: Function): any[] => {
+			if (!params.items) {
+				return null;
+			}
+			let result: (MultiRootExampleSettings | null)[] = [];
+			for (let item of params.items) {
+				// The server asks the client for configuration settings without a section
+				// If a section is present we return null to indicate that the configuration
+				// is not supported.
+				if (item.section) {
+					result.push(null);
+					continue;
+				}
+				let config: WorkspaceConfiguration;
+				if (item.scopeUri) {
+					config = workspace.getConfiguration('lsp-multi-root-sample', client.protocol2CodeConverter.asUri(item.scopeUri));
+				} else {
+					config = workspace.getConfiguration('lsp-multi-root-sample');
+				}
+				result.push({
+					maxNumberOfProblems: config.get('maxNumberOfProblems')
+				});
+			}
+			return result;
+
+		}
+	};
+
+	// Options to control the language client
+	let clientOptions: LanguageClientOptions = {
+		// Register the server for plain text documents
+		documentSelector: [{scheme: 'file', language: 'plaintext'}],
+		synchronize: {
+			// Notify the server about file changes to '.clientrc files contain in the workspace
+			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+		},
+		middleware: {
+			workspace: configurationMiddleware as any // cast to any due to proposed API
+		}
+	}
+	
+	// Create the language client and start the client.
+	let client = new LanguageClient('languageServerExample', 'Language Server Example', serverOptions, clientOptions);
+	// Register new propose protocol if available.
+	client.registerFeatures(ProposedProtocol(client));
+	
+	// Start the client. This will also launch the server
+	let disposable = client.start();
+	
+	// Push the disposable to the context's subscriptions so that the 
+	// client can be deactivated on extension deactivation
+	context.subscriptions.push(disposable);
+}
---
+++ lsp-multi-root-sample/server/src/server.ts
@@ -0,0 +1,125 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+'use strict';
+
+import {
+	IPCMessageReader, IPCMessageWriter,
+	createConnection, IConnection, TextDocumentSyncKind,
+	TextDocuments, TextDocument, Diagnostic, DiagnosticSeverity,
+	InitializeParams, InitializeResult, TextDocumentPositionParams,
+	DidChangeConfigurationNotification, ClientCapabilities,
+	CompletionItem, CompletionItemKind, StreamMessageReader, StreamMessageWriter, ProposedProtocol,
+	ConfigurationItem
+} from 'vscode-languageserver';
+
+// Create a connection for the server. The connection uses Node's IPC as a transport
+let connection = createConnection(ProposedProtocol);
+
+// Create a simple text document manager. The text document manager
+// supports full document sync only
+let documents: TextDocuments = new TextDocuments();
+
+connection.onInitialize((params): InitializeResult => {
+	return {
+		capabilities: {
+			// Tell the client that the server works in FULL text document sync mode
+			textDocumentSync: documents.syncKind,
+			// Tell the client that the server support code complete
+			completionProvider: {
+				resolveProvider: true
+			}
+		}
+	}
+});
+
+// The example settings
+interface MultiRootExampleSettings {
+	maxNumberOfProblems: number;
+}
+
+let settings: Map<string, Thenable<MultiRootExampleSettings>> = new Map();
+
+function getConfiguration(resource: string): Thenable<MultiRootExampleSettings> {
+	let result = settings.get(resource);
+	if (!result) {
+		result = connection.workspace.getConfiguration({ section: '', scopeUri: resource });
+		settings.set(resource, result);
+	}
+	return result;
+}
+
+connection.onInitialized(() => {
+	// Register to configuration change events.
+	connection.client.register(DidChangeConfigurationNotification.type);
+});
+
+
+connection.onNotification(DidChangeConfigurationNotification.type, () => {
+	let toRequest: ConfigurationItem[] = [];
+	for (let resource of settings.keys()) {
+		toRequest.push({ section: '', scopeUri: resource});
+	}
+	settings.clear();
+	// Reread all cached configuration
+	connection.workspace.getConfiguration(toRequest).then((values: MultiRootExampleSettings[]) => {
+		let toRevalidate: string[] = [];
+		for (let i = 0; i < values.length; i++) {
+			let resource = toRequest[i].scopeUri;
+			let value = values[i];
+			// If the value got already added to the settings cache then a change has
+			// occurred before the configuration request got return. Ignore the value
+			// in this case.
+			if (value && !settings.has(resource)) {
+				settings.set(resource, Promise.resolve(value));
+				toRevalidate.push(resource);
+			}
+		}
+		for (let resource of toRevalidate) {
+			validateTextDocument(documents.get(resource));
+		}
+	});
+});
+
+
+// The content of a text document has changed. This event is emitted
+// when the text document first opened or when its content has changed.
+documents.onDidChangeContent((change) => {
+	validateTextDocument(change.document);
+});
+
+
+function validateTextDocument(textDocument: TextDocument): void {
+	// In this simple example we get the settings for every validate run.
+	getConfiguration(textDocument.uri).then((settings: MultiRootExampleSettings) => {
+		let diagnostics: Diagnostic[] = [];
+		let lines = textDocument.getText().split(/\r?\n/g);
+		let problems = 0;
+		for (var i = 0; i < lines.length && problems < settings.maxNumberOfProblems; i++) {
+			let line = lines[i];
+			let index = line.indexOf('typescript');
+			if (index >= 0) {
+				problems++;
+				diagnostics.push({
+					severity: DiagnosticSeverity.Warning,
+					range: {
+						start: { line: i, character: index},
+						end: { line: i, character: index + 10 }
+					},
+					message: `${line.substr(index, 10)} should be spelled TypeScript`,
+					source: 'ex'
+				});
+			}
+		}
+		// Send the computed diagnostics to VSCode.
+		connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
+	});
+}
+
+// Make the text document manager listen on the connection
+// for open, change and close text document events
+documents.listen(connection);
+
+// Listen on the connection
+connection.listen();
