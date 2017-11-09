e871f00999eb0a2c38ecc04dba36b76b31246ee4
Dirk Baeumer <dirkb@microsoft.com>
2017-11-09 08:51:24 +0100

    Fixes #37: Debugging language servers without legacy protocol

--- lsp-sample/client/src/extension.ts
+++ lsp-sample/client/src/extension.ts
@@ -14,7 +14,7 @@ export function activate(context: ExtensionContext) {
 	// The server is implemented in node
 	let serverModule = context.asAbsolutePath(path.join('server', 'server.js'));
 	// The debug options for the server
-	let debugOptions = { execArgv: ["--nolazy", "--debug=6009"] };
+	let debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
 	
 	// If the extension is launched in debug mode then the debug server options are used
 	// Otherwise the run options are used
