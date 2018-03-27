f1412527b460f47207621240e79233f305a44a4b
Benjamin Pasero <benjpas@microsoft.com>
2018-03-27 11:03:01 +0200

    add progress-ts

---
+++ progress-sample/README.md
@@ -0,0 +1,21 @@
+# Progress Sample
+
+This is a sample extension that shows a running progress in the notification area with support for cancellation.
+
+It is not intended as a product quality extension.
+
+- Open the command palette
+- Run "Show Progress"
+- Observe the running task in the notification area
+
+![Show progress in notification area](https://raw.githubusercontent.com/Microsoft/vscode-extension-samples/master/progress-sample/preview.gif)
+
+# How it works, what it shows?
+
+- The extension uses the [`withProgress`](https://code.visualstudio.com/docs/extensionAPI/vscode-api#ProgressOptions) API to show the task in the notification area.
+- Registers a command via `package.json` that will trigger the task
+
+# How to run locally
+
+* `npm run compile` to start the compiler in watch mode
+* open this folder in VS Code and press `F5`
---
+++ progress-sample/src/extension.ts
@@ -0,0 +1,41 @@
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+
+'use strict';
+
+import { ExtensionContext, StatusBarAlignment, window, StatusBarItem, Selection, workspace, TextEditor, commands, ProgressLocation } from 'vscode';
+
+export function activate(context: ExtensionContext) {
+	context.subscriptions.push(commands.registerCommand('extension.startTask', () => {
+		window.withProgress({
+			location: ProgressLocation.Notification,
+			title: "I am long running!",
+			cancellable: true
+		}, (progress, token) => {
+			token.onCancellationRequested(() => {
+				console.log("User canceled the long running operation")
+			});
+
+			setTimeout(() => {
+				progress.report({ percentage: 10, message: "I am long running! - still going..." });
+			}, 1000);
+
+			setTimeout(() => {
+				progress.report({ percentage: 50, message: "I am long running! - still going even more..." });
+			}, 2000);
+
+			setTimeout(() => {
+				progress.report({ percentage: 90, message: "I am long running! - almost there..." });
+			}, 3000);
+
+			var p = new Promise(resolve => {
+				setTimeout(() => {
+					resolve();
+				}, 5000);
+			});
+
+			return p;
+		});
+	}));
+}
