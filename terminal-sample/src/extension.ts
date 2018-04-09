e467ba000141319159ebfec9c5ebeedd610a4b04
Daniel Imms <daimms@microsoft.com>
2018-04-09 06:33:31 -0700

    Remove use of any

--- terminal-sample/src/extension.ts
+++ terminal-sample/src/extension.ts
@@ -49,21 +49,15 @@ export function activate(context: vscode.ExtensionContext) {
 		terminalStack.push(vscode.window.createTerminal(`Ext Terminal #${terminalStack.length + 1}`));
 		getLatestTerminal().sendText("echo 'Sent text immediately after creating'");
 	}));
-
-	// Below coming in version v1.6
 	context.subscriptions.push(vscode.commands.registerCommand('terminalTest.createZshLoginShell', () => {
-		terminalStack.push((<any>vscode.window).createTerminal(`Ext Terminal #${terminalStack.length + 1}`, '/bin/zsh', ['-l']));
+		terminalStack.push(vscode.window.createTerminal(`Ext Terminal #${terminalStack.length + 1}`, '/bin/zsh', ['-l']));
 	}));
 	context.subscriptions.push(vscode.commands.registerCommand('terminalTest.processId', () => {
-		(<any>getLatestTerminal()).processId.then((processId) => {
+		getLatestTerminal().processId.then((processId) => {
 			console.log(`Shell process ID: ${processId}`);
 		});
 	}));
-	if ('onDidCloseTerminal' in <any>vscode.window) {
-		(<any>vscode.window).onDidCloseTerminal((terminal) => {
-			console.log('Terminal closed', terminal);
-		});
-	}
+	vscode.window.onDidCloseTerminal((terminal) => console.log('Terminal closed', terminal));
 
 	function getLatestTerminal() {
 		return terminalStack[terminalStack.length - 1];
