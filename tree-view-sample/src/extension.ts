968130df45bb59d20804a1fea865691e11c2510d
Sandeep Somavarapu <sasomava@microsoft.com>
2018-05-09 15:02:07 +0200

    Use tex document provder

--- tree-view-sample/src/extension.ts
+++ tree-view-sample/src/extension.ts
@@ -4,7 +4,7 @@ import * as vscode from 'vscode';
 
 import { DepNodeProvider } from './nodeDependencies'
 import { JsonOutlineProvider } from './jsonOutline'
-import { FtpExplorer } from './ftpExplorer.fileSystemProvider'
+import { FtpExplorer } from './ftpExplorer.textDocumentContentProvider'
 
 export function activate(context: vscode.ExtensionContext) {
 	const rootPath = vscode.workspace.rootPath;
