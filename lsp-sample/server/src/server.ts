52ac2fc3d067920d8df8896e9f308337ef093ca3
Jens Fischer <jensfischer95@gmail.com>
2018-06-28 13:58:54 +0200

    Add a compound launch config to lsp-sample

--- lsp-sample/server/src/server.ts
+++ lsp-sample/server/src/server.ts
@@ -35,10 +35,10 @@ connection.onInitialize((params: InitializeParams) => {
 	return {
 		capabilities: {
 			textDocumentSync: documents.syncKind,
-            // Tell the client that the server supports code completion
-            completionProvider: {
-                resolveProvider: true
-            }
+			// Tell the client that the server supports code completion
+			completionProvider: {
+				resolveProvider: true
+			}
 		}
 	}
 });
