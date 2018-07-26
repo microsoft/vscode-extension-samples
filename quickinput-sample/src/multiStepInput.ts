0c3e26166dfb0f59724d3c80ee10fd9f46abec8a
Christof Marti <chrmarti@microsoft.com>
2018-07-26 17:29:21 +0200

    Use Uri

--- quickinput-sample/src/multiStepInput.ts
+++ quickinput-sample/src/multiStepInput.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { QuickPickItem, window, Disposable, CancellationToken, QuickInputButton, QuickInput, ExtensionContext, QuickInputButtons } from 'vscode';
+import { QuickPickItem, window, Disposable, CancellationToken, QuickInputButton, QuickInput, ExtensionContext, QuickInputButtons, Uri } from 'vscode';
 
 /**
  * A multi-step input using window.createQuickPick() and window.createInputBox().
@@ -13,12 +13,12 @@ import { QuickPickItem, window, Disposable, CancellationToken, QuickInputButton,
 export async function multiStepInput(context: ExtensionContext) {
 
 	class MyButton implements QuickInputButton {
-		constructor(public iconPath: { light: string; dark: string; }, public tooltip: string) { }
+		constructor(public iconPath: { light: Uri; dark: Uri; }, public tooltip: string) { }
 	}
 
 	const createResourceGroupButton = new MyButton({
-		dark: context.asAbsolutePath('resources/dark/add.svg'),
-		light: context.asAbsolutePath('resources/light/add.svg')
+		dark: Uri.file(context.asAbsolutePath('resources/dark/add.svg')),
+		light: Uri.file(context.asAbsolutePath('resources/light/add.svg')),
 	}, 'Create Resource Group');
 
 	const resourceGroups: QuickPickItem[] = ['vscode-data-function', 'vscode-appservice-microservices', 'vscode-appservice-monitor', 'vscode-appservice-preview', 'vscode-appservice-prod']
