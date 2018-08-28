bfcdd4c405cbdf09f390167e02f8cd6c17a81e4f
Adriean Khisbe <adriean.khisbe@live.fr>
2018-08-28 09:26:55 +0200

    Retrieve commands suggestions from history file

--- quickinput-sample/src/promptCommandWithHistory.ts
+++ quickinput-sample/src/promptCommandWithHistory.ts
@@ -1,8 +1,11 @@
+import * as fs from 'fs';
 import * as path from 'path';
 import { Uri, window, Disposable } from 'vscode';
 import { QuickPickItem } from 'vscode';
 import { workspace } from 'vscode';
 import * as _ from 'lodash';
+
+export const historyPath = `${process.env.HOME}/.vscode-cmd-history`;
 
 /**
  * A command prompt with history
@@ -30,34 +33,42 @@ class InputItem extends CommandItem {
 
 async function pickCommand() {
 	const disposables: Disposable[] = [];
-	const commands: string[] = [
-		'ls',
-		'quit',
-		's/toto/titi/g',
-		'vsplit'
-	];
-	const commandsItems = _.map(commands, (cmd, index) => new HistoryItem(cmd, `(history item ${index})`));
+	let commandsItems = [];
+	let currentValue = undefined;
+	let historyShouldBeUpdated = false;
+
 	try {
 		return await new Promise<string | undefined>((resolve, reject) => {
 			const input = window.createQuickPick<CommandItem>();
 			input.placeholder = 'Type a command';
 			input.items = commandsItems;
+
+
+			const updateQuickPick = value => {
+				if (!value) {
+					input.items = commandsItems;
+					return;
+				}
+				input.items = [
+					new InputItem(value)
+				].concat(
+					commandsItems
+				)
+				// §todo: add autocomplete suggestions
+			}
+
 			disposables.push(
 				input.onDidChangeValue(value => {
-					if (!value) {
-						input.items = commandsItems;
-						return;
-					}
-					input.items = [
-						new InputItem(value)
-					].concat(
-						commandsItems
-					)
-					// §todo: add autocomplete suggestions
+					currentValue = value;
+					updateQuickPick(value);
 				}),
 				input.onDidChangeSelection(items => {
 					const item = items[0];
 					if (item instanceof HistoryItem) {
+						resolve(item.label);
+						input.hide();
+					} else if (item instanceof InputItem) {
+						// record new input in history
 						resolve(item.label);
 						input.hide();
 					}
@@ -68,6 +79,21 @@ async function pickCommand() {
 				})
 			);
 			input.show();
+
+			if (fs.existsSync(historyPath)) {
+				fs.readFile(historyPath, (err, content) => {
+					if (err) {
+						console.error('Could not load file history', err);
+					}
+					historyShouldBeUpdated = true;
+					const commands = content.toString().trimRight().split('\n').reverse();
+					commandsItems = _.map(commands, (cmd, index) => new HistoryItem(cmd, `(history item ${index})`));
+					updateQuickPick(currentValue);
+				});
+			} else {
+				console.log('history file does not exist yet')
+			}
+
 		});
 	} finally {
 		disposables.forEach(d => d.dispose());
