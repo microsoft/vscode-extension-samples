a4ddcddb19cb6f2a4be4c3680c2c90f1532f8088
Johannes Rieken <johannes.rieken@gmail.com>
2018-09-19 17:54:55 +0200

    wip

---
+++ webpack-sample/src/math.ts
@@ -0,0 +1,13 @@
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+
+'use strict';
+
+export function add(a: number, b: number): number {
+    return a + b;
+}
+
+export function sub(a: number, b: number): number {
+    return a - b;
+}
---
+++ webpack-sample/webpack.config.js
@@ -0,0 +1,39 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+//@ts-check
+
+'use strict';
+
+const path = require('path');
+
+/**@type {import('webpack').Configuration}*/
+const config = {
+    target: 'node',
+    entry: './src/extension.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'extension.js',
+        libraryTarget: "commonjs2"
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    devtool: 'source-map',
+    externals: {
+        vscode: "vscode"
+    },
+    module: {
+        rules: [{
+            test: /\.ts$/,
+            exclude: /node_modules/,
+            use: [{
+                loader: 'ts-loader',
+            }]
+        }]
+    },
+}
+
+module.exports = config;
