29b8ae6a1c16244291e937e667ccb64b653b5349
Johannes Rieken <johannes.rieken@gmail.com>
2018-10-25 12:30:37 +0200

    more completion samples

--- completions-sample/src/extension.ts
+++ completions-sample/src/extension.ts
@@ -8,35 +8,45 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	// The most simple completion item provider which 
-	// * registers for text files (`'plaintext'`), and
-	// * return the 'Hello World' and 
-	//   a snippet-based completion item.
 	let registration = vscode.languages.registerCompletionItemProvider('plaintext', {
 
 		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+
+			// a simple completion item which inserts `Hello World!`
+			const simpleCompletion = new vscode.CompletionItem('Hello World!');
+
+			// a completion item that inserts its text as snippet,
+			// the `insertText`-property is a `SnippetString` which we will
+			// honored by the editor.
+			const snippetCompletion = new vscode.CompletionItem('Good part of the day');
+			snippetCompletion.insertText = new vscode.SnippetString('Good ${1|morning,afternoon,evening|}. It is ${1}, right?');
+			snippetCompletion.documentation = new vscode.MarkdownString("Inserts a snippet that lets you select the _appropriate_ part of the day for your greeting.");
+
+			// a completion item that can be accepted by a commit character,
+			// the `commitCharacters`-property is set which means that the completion will
+			// be inserted and then the character will be typed.
+			const commitCharacterCompletion = new vscode.CompletionItem('console');
+			commitCharacterCompletion.commitCharacters = ['.'];
+			commitCharacterCompletion.documentation = new vscode.MarkdownString('Press `.` to get `console.`');
+
+			// a completion item that retriggers IntelliSense when being accepted,
+			// the `command`-property is set which the editor will execute after 
+			// completion has been inserted. Also, the `insertText` is set so that 
+			// a space is inserted after `new`
+			const commandCompletion = new vscode.CompletionItem('new');
+			commandCompletion.kind = vscode.CompletionItemKind.Keyword;
+			commandCompletion.insertText = 'new ';
+			commandCompletion.command = { command: 'editor.action.triggerSuggest', title: 'Re-trigger completions...' };
+
+			// return all completion items as array
 			return [
-				new vscode.CompletionItem('Hello World!'),
-				createSnippetItem()
+				simpleCompletion,
+				snippetCompletion,
+				commitCharacterCompletion,
+				commandCompletion
 			];
 		}
 	});
 
 	context.subscriptions.push(registration);
-
-	function createSnippetItem(): vscode.CompletionItem {
-
-		// Read more here:
-		// https://code.visualstudio.com/docs/extensionAPI/vscode-api#CompletionItem
-		// https://code.visualstudio.com/docs/extensionAPI/vscode-api#SnippetString
-
-		// For SnippetString syntax look here:
-		// https://code.visualstudio.com/docs/editor/userdefinedsnippets#_creating-your-own-snippets
-
-		let item = new vscode.CompletionItem('Good part of the day', vscode.CompletionItemKind.Snippet);
-		item.insertText = new vscode.SnippetString("Good ${1|morning,afternoon,evening|}. It is ${1}, right?");
-		item.documentation = new vscode.MarkdownString("Inserts a snippet that lets you select the _appropriate_ part of the day for your greeting.");
-
-		return item;
-	}
 }
