cf2e61435718cf4d63a99f7d6c822685caa6d88e
Johannes Rieken <johannes.rieken@gmail.com>
2018-11-15 10:41:25 +0100

    more updates #110

--- statusbar-sample/src/extension.ts
+++ statusbar-sample/src/extension.ts
@@ -8,23 +8,34 @@ import * as vscode from 'vscode';
 
 let myStatusBarItem: vscode.StatusBarItem;
 
-export function activate(context: vscode.ExtensionContext) {
+export function activate({ subscriptions }: vscode.ExtensionContext) {
 
+	// register a command that is invoked when the status bar
+	// item is selected
+	const myCommandId = 'sample.showSelectionCount';
+	subscriptions.push(vscode.commands.registerCommand(myCommandId, () => {
+		let n = getNumberOfSelectedLines(vscode.window.activeTextEditor);
+		vscode.window.showInformationMessage(`Yeah, ${n} line(s) selected... Keep going!`);
+	}));
+
+	// create a new status bar item that we can now manage
 	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	myStatusBarItem.command = myCommandId;
+	subscriptions.push(myStatusBarItem);
 
-	context.subscriptions.push(myStatusBarItem);
+	// register some listener that make sure the status bar 
+	// item always up-to-date
+	subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem));
+	subscriptions.push(vscode.window.onDidChangeTextEditorSelection(updateStatusBarItem));
 
-	context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem));
-	context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection(updateStatusBarItem));
-	context.subscriptions.push(vscode.window.onDidChangeTextEditorViewColumn(updateStatusBarItem));
-
+	// update status bar item once at start
 	updateStatusBarItem();
 }
 
 function updateStatusBarItem(): void {
 	let n = getNumberOfSelectedLines(vscode.window.activeTextEditor);
 	if (n > 0) {
-		myStatusBarItem.text = `$(megaphone) ${n} line(s) selected`;;
+		myStatusBarItem.text = `$(megaphone) ${n} line(s) selected`;
 		myStatusBarItem.show();
 	} else {
 		myStatusBarItem.hide();
