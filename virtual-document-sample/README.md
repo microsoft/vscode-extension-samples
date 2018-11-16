7e8bb9e09bb8c5856cb213e9b25181c11e5b453a
Johannes Rieken <johannes.rieken@gmail.com>
2018-11-16 12:16:23 +0100

    add eventing and update command

--- virtual-document-sample/README.md
+++ virtual-document-sample/README.md
@@ -2,7 +2,7 @@
 
 This is a sample extension that shows how to add virtual documents to the editor.
 
-![cowsay](https://github.com/Microsoft/vscode-extension-samples/blob/master/virtual-document-sample/preview.png)
+![cowsay](https://github.com/Microsoft/vscode-extension-samples/blob/master/virtual-document-sample/cowsay.gif)
 
 
 ## VS Code API
--- virtual-document-sample/src/extension.ts
+++ virtual-document-sample/src/extension.ts
@@ -12,6 +12,11 @@ export function activate({ subscriptions }: vscode.ExtensionContext) {
 	// register a content provider for the cowsay-scheme
 	const myScheme = 'cowsay';
 	const myProvider = new class implements vscode.TextDocumentContentProvider {
+
+		// emitter and its event
+		onDidChangeEmitter = new vscode.EventEmitter<vscode.Uri>();
+		onDidChange = this.onDidChangeEmitter.event;
+
 		provideTextDocumentContent(uri: vscode.Uri): string {
 			// simply invoke cowsay, use uri-path as text
 			return cowsay.say({ text: uri.path });
@@ -24,8 +29,24 @@ export function activate({ subscriptions }: vscode.ExtensionContext) {
 		let what = await vscode.window.showInputBox({ placeHolder: 'cowsay...' });
 		if (what) {
 			let uri = vscode.Uri.parse('cowsay:' + what);
-			let doc = await vscode.workspace.openTextDocument(uri);
+			let doc = await vscode.workspace.openTextDocument(uri); // calls back into the provider
 			await vscode.window.showTextDocument(doc, { preview: false });
 		}
 	}));
+
+	// register a command that updates the current cowsay
+	subscriptions.push(vscode.commands.registerCommand('cowsay.backwards', async () => {
+		if (!vscode.window.activeTextEditor) {
+			return; // no editor
+		}
+		let { document } = vscode.window.activeTextEditor;
+		if (document.uri.scheme !== myScheme) {
+			return; // not my scheme
+		}
+		// get path-components, reverse it, and create a new uri
+		let say = document.uri.path;
+		let newSay = say.split('').reverse().join('');
+		let newUri = document.uri.with({ path: newSay });
+		await vscode.window.showTextDocument(newUri, { preview: false });
+	}))
 }
