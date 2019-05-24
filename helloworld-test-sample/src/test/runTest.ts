a0c17bdad3c2c072fa1008c76c7bf0aaf63016a1
Pine Wu <octref@gmail.com>
2019-05-24 01:02:37 -0700

    Update to latest vscode-test API

--- helloworld-test-sample/src/test/runTest.ts
+++ helloworld-test-sample/src/test/runTest.ts
@@ -6,15 +6,12 @@ async function go() {
 	try {
 		const extensionPath = path.resolve(__dirname, '../../');
 		const testRunnerPath = path.resolve(__dirname, './suite');
-		const testWorkspace = path.resolve(__dirname, '../../test/suite/fixture');
 
 		await runTests({
 			// The folder containing the Extension Manifest package.json
 			extensionPath,
 			// The path to test runner
-			testRunnerPath,
-			// The workspace to open on starting up VS Code
-			testWorkspace
+			testRunnerPath
 		});
 	} catch (err) {
 		console.error('Failed to run tests');
