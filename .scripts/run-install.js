7789d15e0a8f67e3fc2604800fb52757d4cd0cc0
Matt Bierner <matb@microsoft.com>
2019-05-31 11:49:59 -0700

    Add install all script

--- .scripts/run-install.js
+++ .scripts/run-install.js
@@ -1,28 +1,26 @@
 // @ts-check
 /**
- * Try running an npm script for each of the samples.
+ * Try running install for all the samples
  */
 const fs = require('fs');
 const path = require('path');
 const child_process = require('child_process');
 const { samples, lspSamples } = require('./samples')
 
-async function tryRun(
-  /** @type {string} */ scriptName,
+async function tryRunInstall(
   /** @type {import('./samples').Sample} */ sample
 ) {
   const packageJsonPath = path.join(sample.path, 'package.json');
   const packageJson = JSON.parse(fs.readFileSync(packageJsonPath).toString());
-  if (Object.keys(packageJson['scripts'] || {}).includes(scriptName)) {
-    console.log(`=== Running ${scriptName} on ${path.basename(sample.path)} ===`)
-    child_process.execSync(`npm run ${scriptName}`, {
+  if (packageJson['devDependencies'] || packageJson['dependencies']) {
+    console.log(`=== Running install on ${path.basename(sample.path)} ===`)
+    child_process.execSync(`npm install`, {
       cwd: sample.path,
       stdio: 'inherit'
     });
   }
 }
 
-const scriptName = process.argv[2];
 for (const sample of [...samples, ...lspSamples]) {
-  tryRun(scriptName, sample);
+  tryRunInstall(sample);
 }
