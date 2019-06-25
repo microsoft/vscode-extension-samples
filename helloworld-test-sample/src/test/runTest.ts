3733516d6c17165e977be4b63773c3df1013fbf3
Pine Wu <octref@gmail.com>
2019-06-24 20:21:14 -0700

    Update test sample

--- helloworld-test-sample/src/test/runTest.ts
+++ helloworld-test-sample/src/test/runTest.ts
@@ -6,14 +6,14 @@ async function main() {
 	try {
 		// The folder containing the Extension Manifest package.json
 		// Passed to `--extensionDevelopmentPath`
-		const extensionPath = path.resolve(__dirname, '../../');
+		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
 
 		// The path to test runner
 		// Passed to --extensionTestsPath
-		const testRunnerPath = path.resolve(__dirname, './suite');
+		const extensionTestsPath = path.resolve(__dirname, './suite');
 
 		// Download VS Code, unzip it and run the integration test
-		await runTests({ extensionPath, testRunnerPath });
+		await runTests({ extensionDevelopmentPath, extensionTestsPath });
 	} catch (err) {
 		console.error('Failed to run tests');
 		process.exit(1);
