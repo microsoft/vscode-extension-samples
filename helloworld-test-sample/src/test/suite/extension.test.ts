199c5d8eeb33eb3493183cfc1c88d8aa1b7a5756
Pine Wu <octref@gmail.com>
2019-09-25 12:20:23 -0700

    Update test sample

--- helloworld-test-sample/src/test/suite/extension.test.ts
+++ helloworld-test-sample/src/test/suite/extension.test.ts
@@ -1,5 +1,4 @@
 import * as assert from 'assert';
-import { before } from 'mocha';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
@@ -7,9 +6,7 @@ import * as vscode from 'vscode';
 // import * as myExtension from '../extension';
 
 suite('Extension Test Suite', () => {
-	before(() => {
-		vscode.window.showInformationMessage('Start all tests.');
-	});
+	vscode.window.showInformationMessage('Start all tests.');
 
 	test('Sample test', () => {
 		assert.equal([1, 2, 3].indexOf(5), -1);
