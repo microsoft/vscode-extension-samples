b55100aff763fbd5a8814e87b0031108a8f9af65
Pine Wu <octref@gmail.com>
2019-11-13 10:42:26 -0800

    Fix formatting

--- helloworld-test-sample/src/test/suite/index.ts
+++ helloworld-test-sample/src/test/suite/index.ts
@@ -5,10 +5,10 @@ import * as glob from 'glob';
 export function run(): Promise<void> {
 	// Create the mocha test
 	const mocha = new Mocha({
-		ui: 'tdd',
+		ui: 'tdd'
 	});
 	mocha.useColors(true);
-	
+
 	const testsRoot = path.resolve(__dirname, '..');
 
 	return new Promise((c, e) => {
