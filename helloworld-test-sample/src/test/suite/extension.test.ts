89f4e29ff4d7b23c4a9d440d4eac36ad24364349
Brett Cannon <54418+brettcannon@users.noreply.github.com>
2019-12-27 13:41:12 -0800

    Fix path to 'extension.ts'
    The default location for `extension.ts` is two levels up, not one.

--- helloworld-test-sample/src/test/suite/extension.test.ts
+++ helloworld-test-sample/src/test/suite/extension.test.ts
@@ -3,7 +3,7 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../extension';
+// import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
