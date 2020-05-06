cd6c33d101297d4db37869a0ef375ec16d57997c
Matt Bierner <matb@microsoft.com>
2020-05-06 12:13:59 -0700

    Updates to target es2019
    Fixes #265
    
    Also excludes dom typings from the lib files so that dom methods are not suggested

--- call-hierarchy-sample/src/extension.ts
+++ call-hierarchy-sample/src/extension.ts
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { FoodPyramidHierarchyProvider } from './FoodPyramidHierarchyProvider';
+import { TextDecoder } from 'util';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
