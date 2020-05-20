83d8af24dcc8705bf1fbd4666ae8a5019c058120
John James Ermitano <johnjamesermitano@yahoo.com>
2020-05-20 13:05:12 +0800

    added notifications and more comments

--- document-insert-sample/src/extension.ts
+++ document-insert-sample/src/extension.ts
@@ -6,116 +6,160 @@ export function activate(context: vscode.ExtensionContext) {
 	// random animal locations
 	const types = [ 'Ocean', 'Desert', 'Grassland', 'Forest', 'Farm', 'Pet', 'Zoo' ];
 
+	// Command: Insert Random Word
 	let insertRandom = vscode.commands.registerCommand('extension.insertRandom', async () => {
 
+		// Clears all notifications.
 		await vscode.commands.executeCommand('notifications.clearAll');
 		const chance = new Chance();
 
 		const randomInt = chance.integer({ min: 0, max: 6 });
 		const randomType = types[randomInt];
+		// Random animal
 		const randomAnimal = chance.animal({ type: randomType }) + '\n';
 
-		// get active text editor
+		// Active text editor instance.
 		const editor = vscode.window.activeTextEditor;
 
+		// Return if there are no active editors.
 		if (!editor) return;
 
+		// Edit active text editor
 		editor.edit((active) => {
 
-			// Defined position of line 1
+			// Line 1 position
 			const position = new vscode.Position(0, 0);
 
 			// Insert randomAnimal in line 1.
 			active.insert(position, randomAnimal);
+		}).then(() => {
+			
+			// Pop animal location in notifications.
+			vscode.window.showInformationMessage(`Location: ${randomType}`);
 		});
-
-		// Pop animal location
-		vscode.window.showInformationMessage(`Location: ${randomType}`);
 	});
 
+	// Command: Insert Random Word in Selected Line
 	let insertRandomSelection = vscode.commands.registerCommand('extension.insertRandomSelection', async () => {
 
+		// Clears all notifications.
 		await vscode.commands.executeCommand('notifications.clearAll');
 		const chance = new Chance();
 
 		const randomInt = chance.integer({ min: 0, max: 6 });
 		const randomType = types[randomInt];
+		// Random animal
 		const randomAnimal = chance.animal({ type: randomType }) + '\n';
-
+		// Active text editor instance.
 		const editor = vscode.window.activeTextEditor;
 
+		// Return if there are no active editors.
 		if (!editor) return;
 
+		// Edit active text editor
 		editor.edit((active) => {
 
-			// Get position of selected line
+			// Position of selected line.
 			const selection = editor.selection.anchor;
 
 			// Insert randomAnimal in selected line.
 			active.insert(selection, randomAnimal);
 
+		}).then(() => {
+			
+			// Pop animal location in notifications.
+			vscode.window.showInformationMessage(`Location: ${randomType}`);
 		});
-
-		vscode.window.showInformationMessage(`Location: ${randomType}`);
 	});
 
+	// Command: Delete Random Word
 	let deleteRandom = vscode.commands.registerCommand('extension.deleteRandom', async () => {
 
+		// Clears all notifications.
 		await vscode.commands.executeCommand('notifications.clearAll');
+		// Active text editor instance.
 		const editor = vscode.window.activeTextEditor;
 
+		// Return if there are no active editors.
 		if (!editor) return;
 
+		// Edit active text editor
 		editor.edit((active) => {
 
-			// Defined range of line 1
+			// Line 0
 			const start = new vscode.Position(0, 0);
+			// Line 1
 			const end = new vscode.Position(1, 0);
+			// Line 1 position
 			const range = new vscode.Range(start, end);
 
-			// Delete text in line 1
+			// Delete text in line 1.
 			active.delete(range);
+		}).then(() => {
+			
+			// Notification
+			vscode.window.showInformationMessage(`Successfully removed text in line 1.`);
 		});
 	});
 
+	// Command: Delete Selected Text
 	let deleteSelection = vscode.commands.registerCommand('extension.deleteSelection', async () => {
 
+		// Clears all notifications.
 		await vscode.commands.executeCommand('notifications.clearAll');
+		// Active text editor instance.
 		const editor = vscode.window.activeTextEditor;
 
+		// Return if there are no active editors.
 		if (!editor) return;
 
+		// Edit active text editor
 		editor.edit((active) => {
 
-			// Get selected range of text
+			// Selected text.
 			const selection = editor.selection;
 
 			// Delete selected text
 			active.delete(selection);
+		}).then(() => {
+			
+			// Notification
+			vscode.window.showInformationMessage(`Successfully removed selected text.`);
 		});
 	});
 
+	// Command: Delete Text in Selected Line
 	let deleteSelectionLine = vscode.commands.registerCommand('extension.deleteSelectionLine', async () => {
 
+		// Clears all notifications.
 		await vscode.commands.executeCommand('notifications.clearAll');
+		// Active text editor instance.
 		const editor = vscode.window.activeTextEditor;
 
+		// Return if there are no active editors.
 		if (!editor) return;
 
+		// Edit active text editor
 		editor.edit((active) => {
 
-			// Get line # of selection
+			// Line number of selected line
 			const startLine = editor.selection.start.line;
 			const endLine = startLine + 1;
 
-			// Define position of selected line
 			const start = new vscode.Position(startLine, 0);
 			const end = new vscode.Position(endLine, 0);
+			// Position of selected line
 			const range = new vscode.Range(start, end);
 
+			// Delete text in selected line
 			active.delete(range);
+		}).then(() => {
+			
+			// Notification
+			vscode.window.showInformationMessage(`Successfully removed text in selected line.`);
 		});
 	});
 
+	// dispose
 	context.subscriptions.push(insertRandom, insertRandomSelection, deleteRandom, deleteSelection, deleteSelectionLine);
 }
