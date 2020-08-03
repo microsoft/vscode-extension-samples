2f9e5fbcdf332f2d226f07803cc4aa73665bb775
RMacfarlane <ramacfar@microsoft.com>
2020-08-03 14:31:36 -0700

    Add github-authentication-sample

---
+++ github-authentication-sample/README.md
@@ -0,0 +1,20 @@
+# GitHub Authentication Sample
+
+This is a GitHub authentication example that shows you how to use VS Code API authentication API.
+
+## Demo
+
+![demo](demo.png)
+
+## VS Code API
+
+### `vscode` module
+
+- [`authentication.getSession`](https://code.visualstudio.com/api/references/vscode-api#authentications.getSession)
+
+## Running the Sample
+
+- Run `npm install` in terminal to install dependencies
+- Run the `Run Extension` target in the Debug View. This will:
+	- Start a task `npm: watch` to compile the code
+	- Run the extension in a new VS Code window
---
+++ github-authentication-sample/src/extension.ts
@@ -0,0 +1,28 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import { Credentials } from './credentials';
+
+
+export async function activate(context: vscode.ExtensionContext) {
+	const credentials = new Credentials();
+	await credentials.initialize(context);
+	
+	const disposable = vscode.commands.registerCommand('extension.getGitHubUser', async () => {
+		/**
+		 * Octokit (https://github.com/octokit/rest.js#readme) is a library for making REST API
+		 * calls to GitHub. It provides convenient typings that can be helpful for using the API.
+		 * 
+		 * Documentation on GitHub's REST API can be found here: https://docs.github.com/en/rest
+		 */
+		const octokit = await credentials.getOctokit();
+		const userInfo = await octokit.users.getAuthenticated();
+
+		vscode.window.showInformationMessage(`Logged into GitHub as ${userInfo.data.login}`);
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
