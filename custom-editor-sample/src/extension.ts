6a756f89df9faa23f125c06f90e0a0af3197f4a1
Matt Bierner <matb@microsoft.com>
2020-11-12 23:17:04 -0800

    Remove version check
    This sample now requires VS Code 1.46+

--- custom-editor-sample/src/extension.ts
+++ custom-editor-sample/src/extension.ts
@@ -5,8 +5,5 @@ import { PawDrawEditorProvider } from './pawDrawEditor';
 export function activate(context: vscode.ExtensionContext) {
 	// Register our custom editor providers
 	context.subscriptions.push(CatScratchEditorProvider.register(context));
-
-	if (+vscode.version.match(/1\.(\d+)/)![1] >= 45) {
-		context.subscriptions.push(PawDrawEditorProvider.register(context));
-	}
+	context.subscriptions.push(PawDrawEditorProvider.register(context));
 }
