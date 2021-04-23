183bc5a557bf9fd10d6f4b7de65f1704a2d0d5dd
Dirk Baeumer <dirkb@microsoft.com>
2021-04-23 18:45:30 +0200

    Add lsif support

---
+++ .scripts/update-lsif.js
@@ -0,0 +1,30 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const { samples, lspSamples } = require('./samples')
+
+const root = path.join(__dirname, '..');
+
+async function main() {
+	const references = [];
+	for (const sample of samples) {
+		try {
+			const stat = await fs.stat(path.join(root, sample.path, 'tsconfig.json'));
+			if (stat.isFile()) {
+				references.push(`../${sample.path}/tsconfig.json`);
+			}
+		} catch (error) {
+			// Ignore error of stat call.
+		}
+	}
+	const tsconfig = {
+		compilerOptions: {
+		},
+		files: [
+		],
+		references: references.map(reference => { return { path: reference }})
+	}
+	await fs.writeFile(path.join(root, '.lsif', 'tsconfig.json'), JSON.stringify(tsconfig, undefined, '\t'), { encoding: 'utf8' });
+}
+
+main().catch(console.error);
