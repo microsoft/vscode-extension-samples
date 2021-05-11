df2011703eb32a11676bcfc9029aebc827be948c
Tyler James Leonhardt <me@tylerleonhardt.com>
2021-05-11 16:31:00 -0700

    comment feedback

--- uri-handler-sample/src/extension.ts
+++ uri-handler-sample/src/extension.ts
@@ -22,8 +22,9 @@ export function activate(context: vscode.ExtensionContext) {
 		// And register it with VS Code. You can only register a single UriHandler for your extension.
 		context.subscriptions.push(vscode.window.registerUriHandler(uriHandler));
 
-		// This gives you the exact Uri to put in your browser. The `vscode.env.asExternalUri` will add a
-		// query parameter (ex: "windowId%3D14") that will help VS Code decide which window to redirect to.
+		// You don't have to get the Uri from the `vscode.env.asExternalUri` API but it will add a query
+		// parameter (ex: "windowId%3D14") that will help VS Code decide which window to redirect to.
+		// If this query parameter isn't specified, VS Code will pick the last windows that was focused.
 		const uri = await vscode.env.asExternalUri(vscode.Uri.parse(`${vscode.env.uriScheme}://vscode-samples.uri-handler-sample`));
 		vscode.window.showInformationMessage(`Starting to handle Uris. Open ${uri} in your browser to test.`);
 	});
