47d64835f7cd7d508824152893418862a322006a
Raymond Zhao <raymondzhao@microsoft.com>
2021-07-07 10:38:15 -0700

    Add repro

--- custom-editor-sample/src/extension.ts
+++ custom-editor-sample/src/extension.ts
@@ -6,4 +6,10 @@ export function activate(context: vscode.ExtensionContext) {
 	// Register our custom editor providers
 	context.subscriptions.push(CatScratchEditorProvider.register(context));
 	context.subscriptions.push(PawDrawEditorProvider.register(context));
+	context.subscriptions.push(vscode.commands.registerCommand('catCustoms.catScratch.new', () => {
+		vscode.workspace.openTextDocument({ language: 'catScratch', content: '' })
+		.then(doc => {
+			vscode.commands.executeCommand("vscode.openWith", doc.uri, "catCustoms.catScratch");
+		});
+	}));
 }
