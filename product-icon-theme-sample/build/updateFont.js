68962383d29dc36134fb174006e3f2f741cb34e2
Martin Aeschlimann <martinae@microsoft.com>
2021-07-09 09:47:02 +0200

    update font generator in product-icon-theme-sample

--- product-icon-theme-sample/build/updateFont.js
+++ product-icon-theme-sample/build/updateFont.js
@@ -1,33 +1,39 @@
-const webfontsGenerator = require('webfonts-generator');
+const webfont = require('webfont');
 const fs = require('fs');
+const path = require('path');
 
 const svgs = [
-    "explorer-view.svg", 
-    "search-view.svg",
-    "debug-view.svg",
-    "git-view.svg",
-    "extensions-view.svg",
-    "smiley.svg",
-    "folding-expanded.svg",
-    "folding-collapsed.svg",
-    "expando-expanded.svg",
-    "expando-collapsed.svg",  
-];
+  "explorer-view.svg",
+  "search-view.svg",
+  "debug-view.svg",
+  "git-view.svg",
+  "extensions-view.svg",
+  "smiley.svg",
+  "folding-expanded.svg",
+  "folding-collapsed.svg",
+  "expando-expanded.svg",
+  "expando-collapsed.svg",
+].map(name => path.join(__dirname, '..', 'icons', name));
 
-webfontsGenerator({
-  files: svgs.map(i => `./icons/${i}`),
-  dest: './out/',
-  types: ['woff'],
-  fontName: 'vscode-10',
-  css: false,
-  html: false,
-  startCodepoint: 0xE000
+async function generateFont() {
 
-}, function(error) {
-  if (error) {
-    console.log('Font creation failed.', error);
-    return;
+  try {
+    const result = await webfont.webfont({
+      files: svgs,
+      formats: ['woff'],
+      startUnicode: 0xE000,
+      verbose: true,
+      normalize: true,
+      sort: false
+    });
+    const dest = path.join(__dirname, '..', 'theme', 'vscode-10.woff')
+    fs.writeFileSync(dest, result.woff, 'binary');
+    console.log(`Font created at ${dest}`);
+  } catch (e) {
+    console.error('Font creation failed.', error);
   }
-  fs.copyFileSync('./out/vscode-10.woff', './theme/vscode-10.woff');
-  console.log('Font created at ./theme/vscode-10.woff');
-})
+}
+
+generateFont();
+
+
