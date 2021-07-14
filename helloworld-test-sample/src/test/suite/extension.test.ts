0f35eb338685ebcf183be02e0c799d0b031bfce2
Logan Ramos <lramos15@gmail.com>
2021-07-14 14:19:44 -0400

    Fix #384

--- helloworld-test-sample/src/test/suite/extension.test.ts
+++ helloworld-test-sample/src/test/suite/extension.test.ts
@@ -9,7 +9,7 @@ suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
 	test('Sample test', () => {
-		assert.equal([1, 2, 3].indexOf(5), -1);
-		assert.equal([1, 2, 3].indexOf(0), -1);
+		assert.strictEqual([1, 2, 3].indexOf(5), -1);
+		assert.strictEqual([1, 2, 3].indexOf(0), -1);
 	});
 });
