e028077bb9d0dfd6da8572b608f85630775dd311
tanhakabir <tanhakabir.ca@gmail.com>
2021-08-01 22:14:27 -0700

    display github issues in a table

--- notebook-renderer-sample/src/client/render.ts
+++ notebook-renderer-sample/src/client/render.ts
@@ -7,8 +7,13 @@ import type { RendererContext } from 'vscode-notebook-renderer';
 interface IRenderInfo {
   container: HTMLElement;
   mime: string;
-  value: any;
+  value: GitHubIssuesValue[];
   context: RendererContext<unknown>;
+}
+interface GitHubIssuesValue {
+	title: string;
+	url: string;
+  body: string;
 }
 
 // This function is called to render your contents.
@@ -17,9 +22,39 @@ export function render({ container, mime, value }: IRenderInfo) {
   // Replace this with your custom code!
   const pre = document.createElement('pre');
   pre.classList.add(style.json);
-  const code = document.createElement('code');
-  code.textContent = `mime type: ${mime}\n\n${JSON.stringify(value, null, 2)}`;
-  pre.appendChild(code);
+
+  // Create a simple table with issue titles and links
+  const table = document.createElement('table');
+  table.className = 'issues-list';
+  const headerRow = document.createElement('tr');
+  const tableHeaders = ['Issue', 'Description'];
+
+  tableHeaders.forEach(label => {
+    const header = document.createElement('th');
+    header.textContent = label;
+    headerRow.appendChild(header);
+  });
+
+  table.appendChild(headerRow);
+
+  value.forEach(item => {
+    const row = document.createElement('tr');
+
+    const title = document.createElement('td');
+    const link = document.createElement('a');
+    link.href = item.url;
+    link.textContent = item.title;
+    title.appendChild(link);
+    row.appendChild(title);
+
+    const body = document.createElement('td');
+    body.textContent = item.body;
+    row.appendChild(body);
+
+    table.appendChild(row);
+  });
+
+  pre.appendChild(table);
   container.appendChild(pre);
 }
 
