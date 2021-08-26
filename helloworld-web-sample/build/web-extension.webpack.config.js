67b00da141152a764509a3b4be5cc923e1e09310
Martin Aeschlimann <martinae@microsoft.com>
2021-08-26 15:26:34 +0200

    update webpack config

--- helloworld-web-sample/build/web-extension.webpack.config.js
+++ helloworld-web-sample/build/web-extension.webpack.config.js
@@ -17,44 +17,50 @@ module.exports = /** @type WebpackConfig */ {
 	mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
 	target: 'webworker', // extensions run in a webworker context
 	entry: {
-		'extension': './src/web/extension.ts',
-		'test/suite/index': './src/web/test/suite/index.ts'
+		extension: './src/web/extension.ts',
+		'test/suite/index': './src/web/test/suite/index.ts',
 	},
 	resolve: {
-		mainFields: ['browser', 'module', 'main'],
+		mainFields: ['browser', 'module', 'main'], // look for `browser` entry point in imported node modules
 		extensions: ['.ts', '.js'], // support ts-files and js-files
 		alias: {
+			// provides alternate implementation for node module and source files
 		},
 		fallback: {
-			'assert': require.resolve('assert')
-		}
+			// Webpack 5 no longer polyfills Node.js core modules automatically.
+			// see https://webpack.js.org/configuration/resolve/#resolvefallback
+			// for the list of Node.js core module polyfills.
+			assert: require.resolve('assert'),
+		},
 	},
 	module: {
-		rules: [{
-			test: /\.ts$/,
-			exclude: /node_modules/,
-			use: [
-				{
-					loader: 'ts-loader'
-				}
-			]
-		}]
+		rules: [
+			{
+				test: /\.ts$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'ts-loader',
+					},
+				],
+			},
+		],
 	},
 	plugins: [
 		new webpack.ProvidePlugin({
-			process: 'process/browser',
+			process: 'process/browser', // provide a shim for the global `process` variable
 		}),
 	],
 	externals: {
-		'vscode': 'commonjs vscode', // ignored because it doesn't exist
+		vscode: 'commonjs vscode', // ignored because it doesn't exist
 	},
 	performance: {
-		hints: false
+		hints: false,
 	},
 	output: {
 		filename: '[name].js',
 		path: path.join(__dirname, '../dist/web'),
-		libraryTarget: 'commonjs'
+		libraryTarget: 'commonjs',
 	},
-	devtool: 'nosources-source-map'
+	devtool: 'nosources-source-map', // create a source map that points to the original source file
 };
