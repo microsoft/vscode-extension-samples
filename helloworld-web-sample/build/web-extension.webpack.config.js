c1271e335e4b228842a8f242b92e0f5854fb13fb
Martin Aeschlimann <martinae@microsoft.com>
2021-09-15 23:58:25 +0200

    polish

--- helloworld-web-sample/build/web-extension.webpack.config.js
+++ helloworld-web-sample/build/web-extension.webpack.config.js
@@ -1,2 +1,1 @@
-// moved to webpack.config.js
-// https://github.com/microsoft/vscode-extension-samples/blob/main/helloworld-web-sample/webpack.config.js
+// moved to helloworld-web-sample/webpack.config.js
