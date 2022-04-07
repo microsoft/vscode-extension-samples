1fedd4b93255be6e03d4be114f375cb53e56d38a
Logan Ramos <lramos15@gmail.com>
2022-04-07 09:27:11 -0400

    Address PR feedback

--- tabs-api-sample/src/extension.ts
+++ tabs-api-sample/src/extension.ts
@@ -8,22 +8,27 @@ export function activate(context: vscode.ExtensionContext) {
 	 * Proposed API as defined in vscode.proposed.<proposalName>.d.ts.
 	 */
 	const activityMap: Map<vscode.Tab, number> = new Map();
-	const disposables: vscode.Disposable[] = [];
 
-	disposables.push(vscode.window.tabGroups.onDidChangeTabGroups(() => {
-		const tabs = vscode.window.tabGroups.groups.map(group => group.tabs).flat(1);
+	context.subscriptions.push(vscode.window.tabGroups.onDidChangeTabGroups(() => {
+		const tabs = vscode.window.tabGroups.all.map(group => group.tabs).flat(1);
 		activityMap.clear();
 		// Update activity map saying everything is active
 		tabs.forEach(t => activityMap.set(t, Date.now()));
 	}));
-	disposables.push(vscode.window.tabGroups.onDidChangeTabs(tabs => {
-		for (const tab of tabs) {
+	context.subscriptions.push(vscode.window.tabGroups.onDidChangeTabs(tabChangeEvent => {
+		// If tabs are closed no longer track their activity
+		for (const removedTab of tabChangeEvent.removed) {
+			activityMap.delete(removedTab);
+		}
+		// Update activity map last active time on changed tabs
+		const changedTabs = [...tabChangeEvent.added, ...tabChangeEvent.changed];
+		for (const tab of changedTabs) {
 			// Reset the timer for the tabs last activity
 			activityMap.set(tab, Date.now());
 		}
 	}));
 	// Check every second for inactive tabs
-	setInterval(() => {
+	const interval = setInterval(() => {
 		const activeTab = vscode.window.tabGroups.activeTabGroup.activeTab;
 		if (activeTab) {
 			// Update the activity map for the active tab, since it's still being used.
@@ -31,12 +36,18 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 		const inactiveTime = (vscode.workspace.getConfiguration().get<number>('tabs.maxInactiveTime') ?? 30) * 1000;
 		// Check if any tabs have been inactive for more than 30 seconds
-		const inactiveTabs = Array.from(activityMap.entries()).filter(([tab, lastActive]) => Date.now() - lastActive > inactiveTime && vscode.window.tabGroups.activeTabGroup.activeTab !== tab && !tab.isDirty);
+		const inactiveTabs = Array.from(activityMap.entries()).filter(([tab, lastActive]) => Date.now() - lastActive > inactiveTime && !tab.isActive && !tab.isDirty);
 		inactiveTabs.forEach(async ([tab]) => {
 			// Close the tab
 			await vscode.window.tabGroups.close(tab);
 			activityMap.delete(tab);
 		});
 	}, 1000);
-	disposables.forEach(d => context.subscriptions.push(d));
+
+	// Create a small disposable to clean up the interval when extension is deactivated
+	context.subscriptions.push({
+		dispose: () => {
+			clearInterval(interval);
+		}
+	});
 }
