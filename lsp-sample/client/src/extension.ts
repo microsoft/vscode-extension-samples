c14bcbb12f422bd3e1ba59e68e6eb776a32ad459
Arichy Xu <arxu@linkedin.com>
2022-08-27 19:28:16 +0800

    chore: replace `--inspect` with `--inspect-brk`

--- lsp-sample/client/src/extension.ts
+++ lsp-sample/client/src/extension.ts
@@ -21,8 +21,8 @@ export function activate(context: ExtensionContext) {
 		path.join('server', 'out', 'server.js')
 	);
 	// The debug options for the server
-	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-	const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+	// --inspect-brk=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging and break at start.
+	const debugOptions = { execArgv: ['--nolazy', '--inspect-brk=6009'] };
 
 	// If the extension is launched in debug mode then the debug server options are used
 	// Otherwise the run options are used
