45531b256a5b39955c8ccff18bc9f8230b406820
Tyler James Leonhardt <me@tylerleonhardt.com>
2022-11-04 12:47:49 -0700

    Add l10n sample/Remove i18n sample (#721)
    * add l10n extension sample, delete vscode-nls sample
    
    * rev l10n versions
    
    * fix a couple typos
    
    * move around sections
    
    * updates
    
    * final
    
    * include link to API

---
+++ l10n-sample/src/cli.ts
@@ -0,0 +1,10 @@
+import * as l10n from '@vscode/l10n';
+
+if (process.env['EXTENSION_BUNDLE_URI']) {
+    l10n.config({
+        uri: process.env['EXTENSION_BUNDLE_URI']
+    });
+}
+
+const message = l10n.t('Hello {0}', 'CLI');
+console.log(message + '\n');
