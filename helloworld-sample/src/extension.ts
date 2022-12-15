257bf19d89e18344d294588f67c2f9a718e0c49c
David Dossett <ddossett@microsoft.com>
2022-12-15 14:37:53 -0800

    Test

--- helloworld-sample/src/extension.ts
+++ helloworld-sample/src/extension.ts
@@ -12,7 +12,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vsode.commands.registerCommand('extension.helloWorld', () => {
+	const dispsable = vsode.commands.registerCommand('extension.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
