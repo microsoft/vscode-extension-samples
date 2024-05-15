11faa662d278aec16f992aa6803e16b5bbd6579b
Rob Lourens <roblourens@gmail.com>
2024-05-15 14:19:20 -0700

    Fix family

--- chat-sample/src/extension.ts
+++ chat-sample/src/extension.ts
@@ -9,7 +9,7 @@ interface ICatChatResult extends vscode.ChatResult {
     }
 }
 
-const LANGUAGE_MODEL_FAMILY = 'copilot-gpt-3.5-turbo'; // Use faster model. Alternative is 'copilot-gpt-4', which is slower but more powerful
+const LANGUAGE_MODEL_FAMILY = 'gpt-3.5-turbo'; // Use faster model. Alternative is 'copilot-gpt-4', which is slower but more powerful
 
 export function activate(context: vscode.ExtensionContext) {
 
