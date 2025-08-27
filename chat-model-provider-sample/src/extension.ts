eeb97cf726c89eb352da7049a3d87e9712348f7d
Logan Ramos <loganramos@microsoft.com>
2025-08-27 15:48:51 -0400

    Update chat model provider sample

--- chat-model-provider-sample/src/extension.ts
+++ chat-model-provider-sample/src/extension.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 import { SampleChatModelProvider } from './provider';
 
 export function activate(_: vscode.ExtensionContext) {
-	vscode.lm.registerChatModelProvider('sample', new SampleChatModelProvider());
+	vscode.lm.registerLanguageModelChatProvider('sample', new SampleChatModelProvider());
 }
 
 export function deactivate() { }
